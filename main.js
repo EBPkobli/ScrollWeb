efb5dce485ef2f6422b1ed7194d121726bb551b2
EBPkobli <41508281+EBPkobli@users.noreply.github.com>
2018-11-27 20:21:30 +0300

    Mobil için güncellendi.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var ctrl_cont = false;
     var exitedImgCount = $(".imgCont").length;
     var indexImg = 1;
+    var animasyonKont = false;
 
     $(".imgCont").each(function () {
         $( this ).css({"top": $( this ).height()+"px", "opacity" : "0"});
@@ -24,58 +25,95 @@ $(document).ready(function(){
 
     //işte wheel eventı burada dönüyor
     $('.container').on('mousewheel', function(event) {
-        if(!ctrl_cont){
+        if(!ctrl_cont && !animasyonKont){
         (event.originalEvent.deltaY < 0) ? wheel_Count-- : wheel_Count++;
         wheel_Count = (wheel_Count < -3) ? 0 : wheel_Count;
         wheel_Count = (wheel_Count > 3) ? 0 : wheel_Count;
         if(wheel_Count == 3)
         {
-            if(indexImg == exitedImgCount) return;
-            var oncekiActive = $(".imgCont.active");
-            $(".imgCont.active").removeClass("active").next().addClass("active");
-            var sonrakiActive = $(".imgCont.active");
-            oncekiActive.animate({
-                top: -oncekiActive.height() +"px",
-                opacity: '0',
-                textIndent: 0.5
-            },{
-                duration : 2000,
-                step: function(now)
-                {
-                    oncekiActive.css('-webkit-transform','scale('+now+')'); 
-                },
-                complete : function(){
-                    oncekiActive.css({"-webkit-transform":"scale(1)","text-indent":"1px"});// , "height" : "100vh"});
-                }
-            })
-            sonrakiActive.animate({
-                top: "0px",
-                opacity: '1'
-            },2000)
-            indexImg++;
+            if(sayfaAnimasyon(1)<0) return;
         }else if(wheel_Count == -3)
         {
-            if(indexImg == 1) return;
-            var oncekiActive = $(".imgCont.active");
-            $(".imgCont.active").removeClass("active").prev().addClass("active");
-            var sonrakiActive = $(".imgCont.active");
-            oncekiActive.animate({
-                top: oncekiActive.height() +"px",
-                width : "25vw" ,
-             // height :"25vh" ,
-                opacity: '0'
-            },2000,function()
-            {
-                oncekiActive.css({"width":"100vw" , "height" : "100vh"});
-            });
-            sonrakiActive.animate({
-                top: "0px",
-                opacity: '1'
-            },2000);
-            indexImg--;
+            if(sayfaAnimasyon(-1)<0) return;
         }
         
     }
     //------------
     });
+
+    //Jquery mobile kütüphanesini buglı ve özensiz bulduğum için 0 dan yazdım
+    //düz javascript ile
+    var el = document.getElementsByClassName("active")[0];
+    var myTouches = new Array();
+    el.addEventListener("touchmove", handleMove, false);
+    function handleMove(evt) {
+        if(animasyonKont) return;
+        evt.preventDefault();
+        var touches = evt.changedTouches;
+        myTouches.push(touches[0].clientY);
+
+        //--------------------
+        if(myTouches[myTouches.length-1]-myTouches[0] > 50){
+            if(sayfaAnimasyon(-1)<0) return;
+            while(myTouches.length > 0) {
+                myTouches.pop();
+            }
+            delete el;
+            el = document.getElementsByClassName("active")[0];
+            el.addEventListener("touchmove", handleMove, false);
+        }
+        //---------------------
+
+        //---------------------
+        else if(myTouches[myTouches.length-1] - myTouches[0] < -50 )
+        {
+            if(sayfaAnimasyon(1)<0) return;
+            while(myTouches.length > 0) {
+                myTouches.pop();
+            }
+            delete el;
+            el = document.getElementsByClassName("active")[0];
+            el.addEventListener("touchmove", handleMove, false);
+        }
+        //---------------------
+        else if(myTouches.length > 10)
+        {
+            while(myTouches.length > 0)
+            {
+                myTouches.pop();
+            }
+        }
+    }
+
+    function sayfaAnimasyon(ileriGeri){
+        if(indexImg == ((ileriGeri >=1) ? exitedImgCount : 1)) return -1;
+        var oncekiActive = $(".imgCont.active");
+        if(ileriGeri>=1)
+            $(".imgCont.active").removeClass("active").next().addClass("active");
+        else
+            $(".imgCont.active").removeClass("active").prev().addClass("active");  
+        var sonrakiActive = $(".imgCont.active");
+        animasyonKont = true;
+        oncekiActive.animate({
+            top: ((ileriGeri >=1) ? -1 : 1) * oncekiActive.height() +"px",
+            opacity: '0',
+            textIndent: 0.5
+        },{
+            duration : 2000,
+            step: function(now)
+            {
+                oncekiActive.css('-webkit-transform','scale('+now+')'); 
+            },
+            complete : function(){
+                oncekiActive.css({"-webkit-transform":"scale(1)","text-indent":"1px"});// , "height" : "100vh"});
+                animasyonKont = false;
+            }
+        })
+        sonrakiActive.animate({
+            top: "0px",
+            opacity: '1'
+        },2000)
+        indexImg += (ileriGeri >=1) ? 1: -1;
+    }
+
 });
